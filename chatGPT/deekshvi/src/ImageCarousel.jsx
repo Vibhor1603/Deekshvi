cfe52b6f0b0e2724eabcdad97c40f27225c6d1ff
Vibhor Sharma <vibhorsharmak@gmail.com>
2024-07-31 23:45:19 +0530

    fourth

--- chatGPT/deekshvi/src/ImageCarousel.jsx
+++ chatGPT/deekshvi/src/ImageCarousel.jsx
@@ -4,12 +4,12 @@ import { Carousel } from 'react-bootstrap';
 
 const ImageCarousel = () => {
   const images = [
-    { src: '../src/assets/first-text.jpg', alt: 'First chat', description: 'The first time deeksha got vibhorified' },
-    { src: '../src/assets/discord-ss2.png', alt: 'first video call', description: 'Thanks to 8 Ball Pool we saw each other for the first time on video call' },
-    { src: '../src/assets/first-meet.jpg', alt: 'first meet up', description: 'One of the most special days in both of our lives, we instantly knew we had something ' },
-    { src: '../src/assets/god-img.jpg', alt: 'I love you deeksha!', description: 'After trying very hard not to, I finally fell for deeksha because I thought I would be an idiot if I just let this gem of a woman vanish  ' },
-    { src: '../src/assets/manali.jpg', alt: 'first trip together', description: 'An experience that brought us even more close , and for me it is and will always be the highlight of my entire life' },
-    { src: '../src/assets/angel-img.jpg', alt: 'many more trips and fights and love making ', description: 'I still fall in love with deeksha everyday for various reasons and that is why I know I made the right choice ' },
+    { src: '/first-text.jpg', alt: 'First chat', description: 'The first time deeksha got vibhorified' },
+    { src: '/discord-ss2.png', alt: 'first video call', description: 'Thanks to 8 Ball Pool we saw each other for the first time on video call' },
+    { src: '/first-meet.jpg', alt: 'first meet up', description: 'One of the most special days in both of our lives, we instantly knew we had something ' },
+    { src: '/god-img.jpg', alt: 'I love you deeksha!', description: 'After trying very hard not to, I finally fell for deeksha because I thought I would be an idiot if I just let this gem of a woman vanish  ' },
+    { src: '/manali.jpg', alt: 'first trip together', description: 'An experience that brought us even more close , and for me it is and will always be the highlight of my entire life' },
+    { src: '/angel-img.jpg', alt: 'many more trips and fights and love making ', description: 'I still fall in love with deeksha everyday for various reasons and that is why I know I made the right choice ' },
 ];
 
   return (
